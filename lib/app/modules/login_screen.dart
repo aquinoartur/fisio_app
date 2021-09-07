ab31d8fe804828dd1639a8365c09b4e02fb3e6b9
aquinoartur <artur-aquino1@hotmail.com>
2021-09-07 15:25:36 -0300

    #4 refactoring

--- lib/app/modules/login_screen.dart
+++ lib/app/modules/login_screen.dart
@@ -30,100 +30,110 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     final primaryColor = Theme.of(context).primaryColor;
     return Scaffold(
-      body: Stack(
-        children: [
-          Positioned.fill(
-            child: Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage("assets/images/login.jpg"),
-                    fit: BoxFit.fitHeight),
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Positioned.fill(
+              child: Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("assets/images/login.jpg"),
+                      fit: BoxFit.fitHeight),
+                ),
               ),
             ),
-          ),
-          Positioned(
-            bottom: MediaQuery.of(context).size.height / 1.1,
-            child: FirstLinearGradientWidget(),
-          ),
-          Positioned(
-            top: MediaQuery.of(context).size.height / 2,
-            child: SecLinearGradientWidget(),
-          ),
-          Positioned.fill(
-            top: MediaQuery.of(context).size.height / 1.4,
-            child: Align(
-                alignment: Alignment.center,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      "Bem-vindo",
-                      style: GoogleFonts.nunito(
+            Positioned(
+              bottom: MediaQuery.of(context).size.height / 1.1,
+              child: FirstLinearGradientWidget(),
+            ),
+            Positioned(
+              top: MediaQuery.of(context).size.height / 2,
+              child: SecLinearGradientWidget(),
+            ),
+            Positioned.fill(
+              top: MediaQuery.of(context).size.height / 1.5,
+              child: Align(
+                  alignment: Alignment.center,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Bem-vindo",
+                        style: GoogleFonts.nunito(
+                            color: primaryColor,
+                            fontSize: 38,
+                            fontWeight: FontWeight.w800),
+                        textAlign: TextAlign.center,
+                      ),
+                      Text(
+                        "Acesse os melhores conteúdos de testes em Fisioterapia.",
+                        style: GoogleFonts.nunito(
+                            color: Colors.black87,
+                            fontSize: 14,
+                            fontWeight: FontWeight.w700),
+                        textAlign: TextAlign.center,
+                      ),
+                      SizedBox(height: 15),
+                      AnimatedOpacity(
+                        duration: const Duration(seconds: 1),
+                        opacity: isLoaded ? 1 : 0,
+                        child: Material(
                           color: primaryColor,
-                          fontSize: 38,
-                          fontWeight: FontWeight.w800),
-                      textAlign: TextAlign.center,
-                    ),
-                    Text(
-                      "Acesse os melhores conteúdos de testes em Fisioterapia.",
-                      style: GoogleFonts.nunito(
-                          color: Colors.black87,
-                          fontSize: 14,
-                          fontWeight: FontWeight.w700),
-                      textAlign: TextAlign.center,
-                    ),
-                    SizedBox(height: 15),
-                    AnimatedOpacity(
-                      duration: const Duration(seconds: 1),
-                      opacity: isLoaded ? 1 : 0,
-                      child: Material(
-                        color: primaryColor,
-                        borderRadius: BorderRadius.circular(30),
-                        child: InkWell(
-                          onTap: () => showDialog(
-                              context: context,
-                              builder: (_) => DialogLogin(),
-                              barrierDismissible: false),
                           borderRadius: BorderRadius.circular(30),
-                          child: Container(
-                            alignment: Alignment.center,
-                            height: 55,
-                            width: 300,
-                            child: Text(
-                              "Login",
-                              style: GoogleFonts.nunito(
-                                  color: Colors.white,
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold),
-                              textAlign: TextAlign.center,
+                          child: InkWell(
+                            onTap: () => showDialog(
+                                context: context,
+                                builder: (_) => DialogLogin(),
+                                barrierDismissible: false),
+                            borderRadius: BorderRadius.circular(30),
+                            child: Container(
+                              alignment: Alignment.center,
+                              height: 55,
+                              width: 300,
+                              child: Text(
+                                "Login",
+                                style: GoogleFonts.nunito(
+                                    color: Colors.white,
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold),
+                                textAlign: TextAlign.center,
+                              ),
                             ),
                           ),
                         ),
                       ),
-                    ),
-                    SizedBox(height: 20),
-                    GestureDetector(
-                      onTap: () => showDialog(
-                        context: context,
-                        builder: (_) => DialogSignout(),
-                        barrierDismissible: false,
+                    ],
+                  )),
+            ),
+            //Positioned.fill(child: ContactsLoginWidget()),
+            Positioned.fill(
+              child: Align(
+                alignment: Alignment.topRight / 1.15,
+                child: GestureDetector(
+                  onTap: () => showDialog(
+                    context: context,
+                    builder: (_) => DialogSignout(),
+                    barrierDismissible: false,
+                  ),
+                  child: Container(
+                    alignment: Alignment.center,
+                    width: 100,
+                    height: 30,
+                    child: Text(
+                      "Criar conta",
+                      style: TextStyles.cardtitle1.copyWith(
+                        color: primaryColor,
+                        fontWeight: FontWeight.w800,
+                        fontSize: 16,
                       ),
-                      child: Container(
-                        alignment: Alignment.center,
-                        width: 100,
-                        height: 30,
-                        child: Text(
-                          "Criar conta.",
-                          style: TextStyles.cardtitle1,
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
+                      textAlign: TextAlign.center,
                     ),
-                  ],
-                )),
-          ),
-          Positioned.fill(child: ContactsLoginWidget())
-        ],
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/app/text_styles/text_styles.dart
+++ lib/app/text_styles/text_styles.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-
 class TextStyles {
-
   static final TextStyle title1 = GoogleFonts.nunito(
     color: Colors.black87.withOpacity(.7),
     fontSize: 18,
@@ -41,25 +39,20 @@ class TextStyles {
   );
 
   static final TextStyle ts1 = GoogleFonts.nunito(
-      color: Color.fromARGB(255, 74,20,140),
-      fontWeight:FontWeight.w600,
-      fontSize: 24
-  );
+      color: Color.fromARGB(255, 74, 20, 140),
+      fontWeight: FontWeight.w600,
+      fontSize: 24);
 
   static final TextStyle ts2 = GoogleFonts.nunito(
       fontSize: 18,
       color: Colors.black.withOpacity(.7),
-      fontWeight: FontWeight.w800
-  );
+      fontWeight: FontWeight.w800);
 
   static final TextStyle ts3 = GoogleFonts.nunito(
       fontSize: 16,
       color: Colors.black.withOpacity(.7),
-      fontWeight: FontWeight.w600
-  );
+      fontWeight: FontWeight.w600);
 
-  static final TextStyle ts4 = GoogleFonts.nunito(
-      color: Colors.white, fontSize: 20
-  );
-
+  static final TextStyle ts4 =
+      GoogleFonts.nunito(color: Colors.white, fontSize: 20);
 }
