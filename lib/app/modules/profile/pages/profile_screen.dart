f5cfe6748bab9d22a194d5232d76683979d7c7e8
aquinoartur <artur-aquino1@hotmail.com>
2021-10-20 23:52:52 -0300

    Dynamic Links

--- lib/app/modules/profile/pages/profile_screen.dart
+++ lib/app/modules/profile/pages/profile_screen.dart
@@ -1,9 +1,13 @@
+import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
 import 'package:fisio_app/app/core/core.dart';
 import 'package:fisio_app/app/fisio_design_system/fisio_design_system.dart';
 import 'package:fisio_app/app/modules/auth/bloc/auth_bloc.dart';
 import 'package:fisio_app/app/modules/auth/bloc/auth_events.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+
+import 'dynamic_links/generate_dynamic_link.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({Key? key}) : super(key: key);
@@ -15,16 +19,45 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen> {
   final bloc = Modular.get<AuthBloc>();
   final themeController = Modular.get<FisioThemeController>();
+  String url = '';
   @override
   void initState() {
     super.initState();
+    initDynamicLinks();
+  }
+
+  void initDynamicLinks() async {
+    final PendingDynamicLinkData? data = await FirebaseDynamicLinks.instance.getInitialLink();
+    final Uri? deepLink = data?.link;
+
+    if (deepLink != null) {
+      handleDynamicLink(deepLink);
+    }
+    FirebaseDynamicLinks.instance.onLink(onSuccess: (PendingDynamicLinkData? dynamicLink) async {
+      final Uri? deepLink = dynamicLink?.link;
+
+      if (deepLink != null) {
+        handleDynamicLink(deepLink);
+      }
+    }, onError: (OnLinkErrorException e) async {
+      print(e.message);
+    });
+  }
+
+  handleDynamicLink(Uri url) {
+    List<String> separatedString = [];
+    separatedString.addAll(url.path.split('/'));
+    if (separatedString[1] == 'home') {
+      Modular.to.navigate('/home/');
+      //* eu poderia passar o separatedString[2] caso minha homepage recebesse algum parametro que fosse recuperado pela navegaçao do modular
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Row(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             IconButton(
@@ -39,6 +72,41 @@ class _ProfileScreenState extends State<ProfileScreen> {
                 setState(() {});
               },
             ),
+            ElevatedButton(
+              onPressed: () async {
+                try {
+                  url = await Utils.buildDynamicLink();
+                } catch (e) {
+                  print(e);
+                }
+                setState(() {});
+              },
+              child: const Text(
+                'Gerar Dynamic Link',
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+            const SizedBox(height: 20),
+            if (url.isNotEmpty)
+              Center(
+                child: RichText(
+                  text: TextSpan(
+                    children: [
+                      TextSpan(text: url, style: const TextStyle(fontSize: 20)),
+                      WidgetSpan(
+                        child: IconButton(
+                          onPressed: () {
+                            Clipboard.setData(
+                              ClipboardData(text: url),
+                            );
+                          },
+                          icon: const Icon(Icons.copy),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
           ],
         ),
       ),
